9dd3b07f4b4e6ff2bccaa6d9e7f2680d29de307c
luisangel2895 <luisangel2895@gmail.com>
2021-04-14 23:46:23 -0500

    fixing desing login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
   <!-- inject:css -->
   <link rel="stylesheet" href="{{asset('css/vertical-layout-light/style.css')}}">
   <!-- endinject -->
-  <link rel="shortcut icon" href="{{asset('images/favicon.png')}}" />
+  <link rel="shortcut icon" href="{{asset('images/logo-mini.png')}}" />
 </head>
 <body>
     <div class="content-wrapper">
